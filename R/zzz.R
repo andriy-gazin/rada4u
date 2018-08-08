6b33f69121f5828bd19e9e80d9a0bcbc46e3ffe1
andriy-gazin <andriy.gazin@gmail.com>
2018-08-09 02:51:36 +0300

    initial commit

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onLoad <- function(...) {
+
+  packageStartupMessage("To work with this package you should set your API key first. \n You can learn how to get your API key at https://rada4you.org/help/data#key")
+
+}
